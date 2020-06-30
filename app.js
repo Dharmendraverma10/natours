ac5959238daaab3f2bf68052cc4a208f82eace12
Dharmendraverma10 <vermad50@gmail.com>
2020-06-30 17:46:53 +0530

    implement some important security features in our application

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
@@ -8,11 +14,46 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 //1. Middleware
+
+//set security HTTP Headers using helmet package
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+//Implementation of limit users from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+//Body parser,reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//Data senitization against NOSQL query injection
+app.use(mongoSanitize());
+//Data senitization aginst XSS
+app.use(xss());
+
+//prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
