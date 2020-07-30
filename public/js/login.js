c317e317536aeee032db6420414bfa85722847e2
Dharmendraverma10 <vermad50@gmail.com>
2020-07-30 09:53:59 +0530

    Add signup functionality in our project

--- public/js/login.js
+++ public/js/login.js
@@ -17,7 +17,7 @@ export const login = async (email, password) => {
       showAlert('success', 'Logged in successfully!');
       window.setTimeout(() => {
         location.assign('/');
-      }, 1500);
+      }, 1000);
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
@@ -31,6 +31,7 @@ export const logout = async () => {
       url: 'http://localhost:3000/api/v1/users/logout',
     });
     if (res.data.status === 'success') location.reload(true);
+    //if (res.data.status === 'success') location.assign('/');
   } catch (err) {
     showAlert('error', 'Error logging out try again.');
   }
